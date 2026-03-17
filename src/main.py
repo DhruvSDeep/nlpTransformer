563ea4e61dc78062c6176b1edbb69b72a3f36c72
DhruvSDeep <dhruvsdeepak@gmail.com>
2026-03-17 17:10:16 +0530

    msg

---
+++ src/main.py
@@ -0,0 +1,3 @@
+import token_and_int_izer
+
+print(len(token_and_int_izer.tokenize_midi(".\data\maestro\\2004\MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.midi")))
