9b31de25c55793c6b017884ba901ebcf0fce81da
DhruvSDeep <dhruvsdeepak@gmail.com>
2026-03-30 19:52:16 +0530

    use model_weights.pt, its the best trained model so far.

--- src/generate.py
+++ src/generate.py
@@ -7,7 +7,7 @@ from transformerLogic import EMBED_DIM, NUM_HEADS, NUM_LAYERS, FF_DIM
 def generate(model, seed, manual, token_to_idx, idx_to_token):
     seedTokens = dataLogic.tokenise(seed, manual)
     seedIds = [token_to_idx.get(t, token_to_idx.get('<PAD>', 0)) for t in seedTokens]
-    generated_ids = transformerLogic.creation(model, seed=seedIds, max_length=200, temperature=0.60, topK=30)
+    generated_ids = transformerLogic.creation(model, seed=seedIds, max_length=300, temperature=0.60, topK=40)
     tokens = [idx_to_token.get(idx, "") for idx in generated_ids]
     final_text = "".join(tokens).replace('_', ' ')
     
@@ -25,10 +25,10 @@ idx_to_token = {i: t for t, i in token_to_idx.items()}
 VOCAB_SIZE = len(token_to_idx)
 
 model = transformerLogic.transformer(VOCAB_SIZE, EMBED_DIM, NUM_HEADS, NUM_LAYERS, FF_DIM)
-model.load_state_dict(torch.load("./checkpoints/model_weights_of_epoch_{epoch}.pt"))
+model.load_state_dict(torch.load("./checkpoints/model_weights.pt"))
 
 prompt = """
-They never lie. But, 
+A story does not need a start or an end. Only, a beginning.
 """
 output = generate(model, prompt, manual, token_to_idx, idx_to_token)
 print(output)
